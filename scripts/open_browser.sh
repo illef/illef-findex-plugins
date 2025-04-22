ba3a07d7d95972d43aaa790216ffd10eb69eb614
illef <sai.illef@gmail.com>
2025-04-23 00:41:15 +0900

    use zen-browser

--- scripts/open_browser.sh
+++ scripts/open_browser.sh
@@ -4,14 +4,14 @@ URL=$1
 ID=$2
 
 current_workspace_num=$(xprop -root -notype _NET_CURRENT_DESKTOP | awk '{print $3}')
-exist_browser=$(wmctrl -l | grep "\s$current_workspace_num\s" | grep Brave | wc -l)
+exist_browser=$(wmctrl -l | grep "\s$current_workspace_num\s" | grep Zen | wc -l)
 
 # workspace가 1번만 존재할 경우 xprop -root -notype _NET_CURRENT_DESKTOP은 "not found." 가 된다
 # 위 경우 "$current_workspace_num" 는 "found." 가 되고 이때는 무조건 --new-tab으로 open 하면 된다
 if (( exist_browser > 0 )) || [[ "$current_workspace_num" == "found." ]] ; then
-    brave $1
+    zen-browser $1
 else
-    brave --new-window $1
+    zen-browser --new-window $1
 fi
 
 # 최근 방문한 사이트 정보를 남긴다
